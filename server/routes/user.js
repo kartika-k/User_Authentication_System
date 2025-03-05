d92e705becd962f9298ba11636a37fbbf3adbc20
Kartika Kannojiya <124906223+kartika-k@users.noreply.github.com>
2025-03-05 18:14:24 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -79,6 +79,21 @@ router.post('/forgot-password', async (req, res) => {
     }
 });
 
+router.post('/reset-password/:token',async(req,res) => {
+    const {token} = req.params;
+    const {password} = req.body;
+
+    try{
+        const decoded = await jwt.verify(token,PromiseRejectionEvent.env.KEY);
+        const id = decoded.id;
+        const hashPassword = await bcrypt.hash(password,10);
+        await User.findByIdAndUpdate({_id:id},{password:hashPassword})
+        return res.json({status:true,message:"Password reset successfully"})
+    }catch(err){
+        return res.json("invalid token")
+    }
+})
+
 const verifyUser = async (req, res, next) => {
     try {
         const token = req.cookies.token;
